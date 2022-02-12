228b70ca312ac9188a6b96b25571b07383f3da32
UMAR <UMAR@DESKTOP-6JR5J50>
2022-02-12 20:03:44 +0530

    lab1

---
+++ Lab-book/src/lab_book1/Addition.java
@@ -0,0 +1,14 @@
+package lab_book1;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("ENter two number:");
+		int first=100;
+		int second=200;
+		System.out.println(first+ " " +second);
+		int sum=first+second;
+		System.out.println("The sum is: "+sum);
+	}
+
+}
