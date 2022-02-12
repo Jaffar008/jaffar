498037e34c98f368207e8d7467ad77016b2d4f1a
UMAR <UMAR@DESKTOP-6JR5J50>
2022-02-12 21:46:15 +0530

    lab1

---
+++ Lab-book/src/lab_book1/Vowel.java
@@ -0,0 +1,15 @@
+package lab_book1;
+
+public class Vowel {
+
+	public static void main(String[] args) {
+		char ch='o';
+		if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u') {
+			System.out.print(ch+" is a vowel");
+		}
+		else {
+			System.out.println(ch+" is consonant");
+		}
+	}
+
+}
