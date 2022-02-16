5c1ff65561d2c7c88689049ab88680220ac79fa8
UMAR <UMAR@DESKTOP-6JR5J50>
2022-02-16 20:36:35 +0530

    lab1

---
+++ Lab-book/src/lab_book1/SwitchCase.java
@@ -0,0 +1,20 @@
+package lab_book1;
+
+public class SwitchCase {
+public static void main(String[] args) {
+	int tech=2;
+	switch(tech) {
+	case 1:{
+		System.out.println("java");
+		break;
+	}
+	case 2:{
+		System.out.println("ES6");
+		break;
+	}
+	default:{
+		System.out.println("Not listed...");
+	}
+	}
+}
+}
