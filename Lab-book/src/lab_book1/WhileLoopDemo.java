2532edf86169e71f4d691f7f8faff46fc840d4b2
UMAR <UMAR@DESKTOP-6JR5J50>
2022-02-16 20:20:56 +0530

    lab1

---
+++ Lab-book/src/lab_book1/WhileLoopDemo.java
@@ -0,0 +1,15 @@
+package lab_book1;
+import java.util.Scanner;
+public class WhileLoopDemo {
+public static void main(String[] args) {
+	int number,sum=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("\n Please Enter any integer value below 10: ");
+	number=sc.nextInt();
+	while(number<=10) {
+		sum=sum+number;
+		number++;
+	}
+	System.out.format("Sum of the numbers From while Loop is : %d",sum);
+}
+}
