2d95a00bdebd3c46f8711b2d8a8230fe36f65e23
UMAR <UMAR@DESKTOP-6JR5J50>
2022-02-16 20:47:24 +0530

    lab1

---
+++ Lab-book/src/lab_book1/FindAreaOfRectangle.java
@@ -0,0 +1,19 @@
+package lab_book1;
+
+import java.util.Scanner;
+
+public class FindAreaOfRectangle {
+	public static void main (String[] args)
+	{
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the length of Rectangle:");
+	double length = scan.nextDouble();
+	System.out.println("Enter the width of Rectangle:");
+	double width = scan.nextDouble();
+	//Area = length*width;
+
+	double area = length*width;
+
+	System.out.println("Area of Rectangle is:"+area);
+	}
+}
